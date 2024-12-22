49f4e63110c0b26c5776e68c6e1b6a5ae2811bd8
michal hanau <mh3182100@gmail.com>
2024-12-23 00:59:29 +0200

    BASE_URL

--- src/services/httpService.ts
+++ src/services/httpService.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance  } from 'axios';
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 const api: AxiosInstance = axios.create({
-  baseURL: `https://miriclinic-server.onrender.com/api`,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
